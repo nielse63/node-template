63dd9af69c0f447dc06791b7ce5632250c107676
Erik Nielsen <erik@312development.com>
2021-03-28 00:58:30 -0500

    feat: configured release-it

--- .release-it.js
+++ .release-it.js
@@ -1,8 +1,13 @@
 /* eslint-disable no-template-curly-in-string */
 
 module.exports = {
+  hooks: {
+    'before:init': ['npm run lint', 'npm test'],
+  },
   git: {
     commitMessage: 'chore: release v${version}',
+    requireCleanWorkingDir: true,
+    requireBranch: 'master',
   },
   github: {
     release: true,
