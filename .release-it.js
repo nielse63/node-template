9e3649ca3f31854b18c555a99f5e12e5358d84ad
Erik Nielsen <erik@312development.com>
2021-03-28 01:05:23 -0500

    Updates (#5)
    * chore: updated dependencies
    
    * feat: added new github actions
    
    * chore: formatted files
    
    * feat: adding release script
    
    * feat: configured release-it
    
    * chore: set node version in actions

---
+++ .release-it.js
@@ -0,0 +1,18 @@
+/* eslint-disable no-template-curly-in-string */
+
+module.exports = {
+  hooks: {
+    'before:init': ['npm run lint', 'npm test'],
+  },
+  git: {
+    commitMessage: 'chore: release v${version}',
+    requireCleanWorkingDir: true,
+    requireBranch: 'master',
+  },
+  github: {
+    release: true,
+  },
+  npm: {
+    publish: false,
+  },
+};
